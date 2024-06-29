355902d7b71e125bcf1cbb1cb2c0ba8ddd8405e7
deboracaires <87550053+deboracaires@users.noreply.github.com>
2024-06-29 16:28:11 -0300

    Criacao de classe Cliente e Teste Cliente
    Co-authored-by: dartmol203 <andre.correa.silva203@gmail.com>
    Co-authored-by: MarianaPRio <marianapires00@gmail.com>
    Co-authored-by: gabrielm2q <mariano.gabriel2q@gmail.com>

---
+++ TPPE-Trabalho1/src/app/Cliente.java
@@ -0,0 +1,52 @@
+package app;
+
+public class Cliente {
+    private int id;
+    private String nome;
+    private boolean ehPrime; // 1 padrao, 2 especial, 3 prime
+    private int estado;
+    private boolean ehCapital;
+    private int saldoCashback = 0; // valor em centavos   
+    
+    /// buscar na de vendas as compras do cliente pra determinar o tipo
+
+    public Cliente(int id, String nome, int estado, boolean ehCapital, boolean ehPrime) {
+        this.id = id;
+        this.nome = nome;
+        this.estado = estado;
+        this.ehCapital = ehCapital;
+        this.ehPrime = ehPrime;
+    }
+
+
+
+	public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public boolean getEhPrime() {
+        return ehPrime;
+    }
+
+    public int getEstado() {
+		return estado;
+	}
+    
+    public boolean getEhCapital() {
+		return ehCapital;
+	}
+
+
+    public double getSaldoCashback() {
+        return saldoCashback;
+    }
+
+    public void adicionarCashback(int valor) {
+        saldoCashback += valor;
+    }
+}
+
