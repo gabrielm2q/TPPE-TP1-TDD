66fdb177f9b030356bf303d14569b21387d42fdd
gabrielm2q <mariano.gabriel2q@gmail.com>
2024-07-07 10:49:52 -0300

    :construction: adds verificaCartaoCredito func
    Co-authored-by: dartmol203 <andre.correa.silva203@gmail.com>

--- TPPE-Trabalho1/src/app/Venda.java
+++ TPPE-Trabalho1/src/app/Venda.java
@@ -10,11 +10,13 @@ public class Venda {
 	private Cliente cliente;
 	private int valorTotal;
 	private LocalDateTime data;
-//	private String metodoPagamento;
-//	private int valorFrete;
-//	private int desconto;
-//	private int ICMS;
-//	private int municipal;
+	private String metodoPagamento; // "PIX", "DEBITO", "CREDITO", "BOLETO"
+	private boolean cartaoCreditoEhDaLoja;
+	private int valorFrete;
+	private int desconto;
+	private int ICMS;
+	private int municipal;
+	private int valorFinal;
 //	valorFinal =  (valorTotal *1,1)*0,9
 //    100 > 110
 //    90 > 100
@@ -22,12 +24,22 @@ public class Venda {
 	
 	// descontos
 
-	public Venda(int id, ArrayList<ProdutoVenda> produtos, Cliente cliente, LocalDateTime data) {
+	public Venda(int id, ArrayList<ProdutoVenda> produtos, Cliente cliente, LocalDateTime data, String metodoPagamento) {
 		super();
 		this.id = id;
 		this.produtos = produtos;
 		this.cliente = cliente;
 		this.data = data;
+		this.metodoPagamento = metodoPagamento;
+	}
+	
+	public Venda(int id, ArrayList<ProdutoVenda> produtos, Cliente cliente, LocalDateTime data, String metodoPagamento, String cartaoCredito) {
+		super();
+		this.id = id;
+		this.produtos = produtos;
+		this.cliente = cliente;
+		this.data = data;
+		this.metodoPagamento = metodoPagamento;
 	}
 	
 	public int getId() {
@@ -62,6 +74,12 @@ public class Venda {
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+	
+	public boolean verificaCartaoCredito(String cartaoCredito) {
+		cartaoCredito = ( cartaoCredito == null || cartaoCredito.length() < 16 ? "XXXX XXXX XXXX XXXX" : cartaoCredito.replaceAll("\s", "") );
+		if ( this.metodoPagamento.equals("CREDITO") && cartaoCredito.substring(0, 7).equals("429613") ) return true;
+		return false;
 	}
 
 	
---
+++ TPPE-Trabalho1/src/teste/TesteVendaVerificaCartaoCredito.java
@@ -0,0 +1,73 @@
+package teste;
+
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import java.util.Collection;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.Test;
+
+import app.Cliente;
+import app.Produto;
+import app.ProdutoVenda;
+import app.Venda;
+
+@RunWith(Parameterized.class)
+public class TesteVendaVerificaCartaoCredito {
+
+	private Venda venda;
+	private int id;
+	private ArrayList<ProdutoVenda> produtos;
+	private Cliente cliente;
+	private String metodoPagamento;
+	private LocalDateTime data;
+	private String cartaoCredito;
+	private boolean cartaoCreditoEhDaLoja;
+	
+	public TesteVendaVerificaCartaoCredito (int id, ArrayList<ProdutoVenda> produtos, Cliente cliente, LocalDateTime data, String metodoPagamento, String cartaoCredito, boolean cartaoCreditoEhDaLoja) {
+		this.id = id;
+		this.produtos = produtos;
+		this.cliente = cliente;
+		this.metodoPagamento = metodoPagamento;
+		this.data = data;
+		this.cartaoCredito = cartaoCredito;
+		this.cartaoCreditoEhDaLoja = cartaoCreditoEhDaLoja;
+	}
+	
+	@Parameters
+	public static Collection<Object[]> getParameters() {
+		Produto produto1 = new Produto(1, "areia", 15000 , "m3");
+		ProdutoVenda produtoVenda1 = new ProdutoVenda(produto1, 1);
+		LocalDateTime date1 = LocalDateTime.now().minusDays(1);
+		Cliente cliente1 = new Cliente(1, "Paulinho Antonio", 2 , true, true);
+		
+		ArrayList<ProdutoVenda> produtos1 = new ArrayList<ProdutoVenda>();
+		produtos1.add(produtoVenda1);
+		
+		Object[][] parametros = new Object[][] {
+			{1, produtos1, cliente1, date1, "PIX", "", false},
+			{2, produtos1, cliente1, date1, "DEBITO", "", false},
+			{3, produtos1, cliente1, date1, "PIX", "1234 5678 9123 4567", false},
+			{4, produtos1, cliente1, date1, "CREDITO", "1234 5678 9123 4567", false},
+			{5, produtos1, cliente1, date1, "CREDITO", "1234", false},
+			{6, produtos1, cliente1, date1, "CREDITO", "4296 1311 1111 2222", false},
+		};
+		
+		return Arrays.asList(parametros);
+	}
+	
+	@Test 
+	public void calcVendaAttributes() {
+		if ( cartaoCredito.equals("") ) {
+			venda = new Venda(id, produtos, cliente, data, metodoPagamento);
+		} else {
+			venda = new Venda(id, produtos, cliente, data, metodoPagamento, cartaoCredito);
+		}
+		
+		assertEquals(venda.verificaCartaoCredito(cartaoCredito), cartaoCreditoEhDaLoja);
+	}
+
+}
